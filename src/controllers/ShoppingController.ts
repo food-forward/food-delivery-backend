f95068774276dc8766c040fcd622b58ccf591737
nelson frank <nelsonfr78@outlook.com>
2023-01-12 13:06:37 +0300

    feat: Add GetFoodAvailability controller

--- src/controllers/ShoppingController.ts
+++ src/controllers/ShoppingController.ts
@@ -1,6 +1,21 @@
 import express, { Request, Response } from "express";
+import { Vendor } from "@/models/Vendor";
 
-export const GetFoodAvailability = async (req: Request, res: Response) => {};
+export const GetFoodAvailability = async (req: Request, res: Response) => {
+  const pincode = req.params.pincode;
+
+  const result = await Vendor.find({
+    pincode: pincode,
+    serviceAvailable: true,
+  })
+    .sort([["rating", "descending"]])
+    .populate("foods");
+
+  if (!result)
+    return res.status(400).json({ message: "Fail to get available food" });
+
+  return res.status(200).json(result);
+};
 
 export const GetTopRestaurants = async (req: Request, res: Response) => {};
 
